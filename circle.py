79a11d69ef06fdc489b18477ae991c56a30691e9
Shezoid <sctrgleb@gmail.com>
2024-11-14 17:07:55 +0300

    added lab

--- circle.py
+++ circle.py
@@ -3,8 +3,8 @@ import math
 
 def area(r):
     return math.pi * r * r
-
+    '''Принимает r, возводит в квадрат и домножает на pi, возвращает площадь круга'''
 
 def perimeter(r):
     return 2 * math.pi * r
-
+    '''Принимает r, возвращает длину окружности'''
